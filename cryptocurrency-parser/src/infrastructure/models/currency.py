25375aa897ac8716db0c25574099724efe493262
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-09 21:57:10 +0200

    Currency ORM model

---
+++ cryptocurrency-parser/src/infrastructure/models/currency.py
@@ -0,0 +1,12 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from infrastructure.models.base import Base
+
+
+class Currency(Base):
+    __tablename__ = "currency"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    ticker: Mapped[str] = mapped_column(String(4))
+    full_name: Mapped[str] = mapped_column(String(20))
