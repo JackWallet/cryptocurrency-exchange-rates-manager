c8f05904c4da39b44f3329e65136bb7747f92c31
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-09 20:50:54 +0200

    Change module name from coin to currency

--- cryptocurrency-parser/src/domain/models/traiding_pair/traiding_pair.py
+++ cryptocurrency-parser/src/domain/models/traiding_pair/traiding_pair.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
 from datetime import datetime
 from domain.models.traiding_pair.traiding_pair_id import TraidingPairId
-from domain.models.coin.coin_id import CoinId
+from domain.models.currency.currency_id import CurrencyId
+
 
 @dataclass
 class TraidingPair:
     id: TraidingPairId
-    base_currency_id: CoinId
-    quote_currency_id: CoinId
+    base_currency_id: CurrencyId
+    quote_currency_id: CurrencyId
     created_at: datetime
