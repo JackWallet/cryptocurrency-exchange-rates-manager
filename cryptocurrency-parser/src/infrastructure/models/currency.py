d91327e9d721859c48ae332b48f1eca65ae6805b
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-10 23:34:03 +0200

    add more columns to the currency table

--- cryptocurrency-parser/src/infrastructure/models/currency.py
+++ cryptocurrency-parser/src/infrastructure/models/currency.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Optional
+
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -10,3 +13,6 @@ class Currency(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     ticker: Mapped[str] = mapped_column(String(4))
     full_name: Mapped[str] = mapped_column(String(20))
+    max_supply: Mapped[Optional[int]]
+    circulating_supply: Mapped[float]
+    last_updated: Mapped[datetime]
