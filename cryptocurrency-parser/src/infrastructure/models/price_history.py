19bb65de2a67ce2bd28797e9f9935ea54f17b4ff
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-10 23:42:38 +0200

    ORM model for the price history table

---
+++ cryptocurrency-parser/src/infrastructure/models/price_history.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from decimal import Decimal
+
+from sqlalchemy import ForeignKey, Numeric
+from sqlalchemy.orm import Mapped, mapped_column
+
+from infrastructure.models.base import Base
+
+
+class PriceHistory(Base):
+    __tablename__ = "price_history"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    currency_id: Mapped[int] = mapped_column(ForeignKey("currency.id"), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(nullable=False)
+    market_cap: Mapped[Decimal] = mapped_column(Numeric(precision=24, scale=16) ,nullable=False)
+    marker_cap_dominance: Mapped[float] = mapped_column(nullable=False)
+    price: Mapped[Decimal] = mapped_column(Numeric(precision=24, scale=24), nullable=False)
+    volume_24h: Mapped[Decimal] = mapped_column(Numeric(precision=24, scale=16))
+    percent_change_1h: Mapped[float] = mapped_column(nullable=False)
+    percent_change_24h: Mapped[float] = mapped_column(nullable=False)
+    percent_change_30d: Mapped[float] = mapped_column(nullable=False)
+    percent_change_60d: Mapped[float] = mapped_column(nullable=False)
+    percent_change_7d: Mapped[float] = mapped_column(nullable=False)
+    percent_change_90d: Mapped[float] = mapped_column(nullable=False)
