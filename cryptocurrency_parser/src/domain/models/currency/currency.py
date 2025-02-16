7a8a1e98853b7665fa4bc12cc44ac9e268d1e057
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-16 17:43:31 +0200

    Add new attributes to Currency

--- cryptocurrency_parser/src/domain/models/currency/currency.py
+++ cryptocurrency_parser/src/domain/models/currency/currency.py
@@ -1,7 +1,11 @@
+from datetime import datetime
 from domain.models.currency.currency_id import CurrencyId
 
 
 class Currency:
     id: CurrencyId
     ticker: str
-    name: str
+    full_name: str
+    max_supply: float | None
+    circulating_supply: float
+    last_updated: datetime
