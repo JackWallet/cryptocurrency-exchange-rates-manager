c65d421c1082ec171ee26694c6d058b435029a05
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-16 23:32:04 +0200

    Made id optional

--- cryptocurrency_parser/src/domain/models/currency/currency.py
+++ cryptocurrency_parser/src/domain/models/currency/currency.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 from domain.models.currency.currency_id import CurrencyId
 
 @dataclass
 class Currency:
-    id: CurrencyId
+    id: Optional[CurrencyId]
     ticker: str
     full_name: str
     last_updated: datetime
