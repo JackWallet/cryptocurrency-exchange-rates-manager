d0d22eb228ffab1daa7a0011933810234af04dce
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-16 17:23:25 +0200

    Add price history domain dataclass

---
+++ cryptocurrency_parser/src/domain/models/price_history/price_history.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
+
+from cryptocurrency_parser.src.domain.models.currency.currency_id import CurrencyId
+from cryptocurrency_parser.src.domain.models.price_history.price_history_id import PriceHistoryId
+
+@dataclass
+class PriceHistory:
+    id: PriceHistoryId
+    currency_id: CurrencyId
+    updated_at: datetime
+    market_cap: Decimal
+    market_cap_dominance: float
+    price: Decimal
+    volume_24h: Decimal
+    percent_change_1h: float
+    percent_change_24h: float
+    percent_change_30d: float
+    percent_change_60d: float
+    percent_change_7d: float
+    percent_change_90d: float
---
+++ cryptocurrency_parser/src/domain/models/price_history/price_history_id.py
@@ -0,0 +1,3 @@
+from typing import NewType
+
+PriceHistoryId = NewType("PriceHistoryId", int)
