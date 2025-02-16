0711eabbe0346d3dbdb2ef47789d9670fc1cc1f2
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-16 17:44:00 +0200

    Add dataclass decorator

--- cryptocurrency_parser/src/domain/models/currency/currency.py
+++ cryptocurrency_parser/src/domain/models/currency/currency.py
@@ -1,7 +1,8 @@
+from dataclasses import dataclass
 from datetime import datetime
 from domain.models.currency.currency_id import CurrencyId
 
-
+@dataclass
 class Currency:
     id: CurrencyId
     ticker: str
