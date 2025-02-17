fa71d52d922be560532eb77c5f107c864c0cea29
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-17 22:43:18 +0200

    add_currency use case

---
+++ cryptocurrency_parser/src/application/interactors/currency/add_currency.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from typing import Optional, Protocol
+
+from application.common.interactor import Interactor
+from application.currency.currency_gateway import CurrencyWriter
+from cryptocurrency_parser.src.domain.models.currency.currency import Currency
+from domain.services.currency.currency import CurrencyService
+
+
+class CurrencyDbGateway(CurrencyWriter, Protocol):
+    pass
+
+
+@dataclass
+class NewCurrencyDTO:
+    ticker: str
+    full_name: str
+    max_supply: Optional[int]
+    circulating_supply: int
+
+
+class AddCurrency(Interactor[NewCurrencyDTO, None]):
+    def __init__(
+        self, currency_db_gateway: CurrencyDbGateway, currency_service: CurrencyService
+    ) -> None:
+        self._currency_db_gateway = currency_db_gateway
+        self._currency_service = currency_service
+
+    async def __call__(self, data: NewCurrencyDTO) -> None:
+        new_currency: Currency = self._currency_service.create_currency(
+            full_name=data.full_name,
+            ticker=data.ticker,
+        )
+
+        await self._currency_db_gateway.save_currency(new_currency)
