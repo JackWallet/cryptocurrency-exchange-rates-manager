e8b7813bb3918699b1f7a2f2c267006c4039ab58
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-17 22:12:17 +0200

    Add max and circulating supply attributes

--- cryptocurrency_parser/src/domain/models/price_history/price_history.py
+++ cryptocurrency_parser/src/domain/models/price_history/price_history.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
+from typing import Optional
 
 from cryptocurrency_parser.src.domain.models.currency.currency_id import CurrencyId
 from cryptocurrency_parser.src.domain.models.price_history.price_history_id import PriceHistoryId
@@ -14,6 +15,8 @@ class PriceHistory:
     market_cap_dominance: float
     price: Decimal
     volume_24h: Decimal
+    max_supply: Optional[int]
+    circulating_supply: int
     percent_change_1h: float
     percent_change_24h: float
     percent_change_30d: float
