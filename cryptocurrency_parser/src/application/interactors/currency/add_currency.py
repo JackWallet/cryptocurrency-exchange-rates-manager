4af58cf734063362f74fe47ef04b742868f8b14f
InWamos <mykyta.pidgornyi@gmail.com>
2025-02-23 21:21:50 +0200

    Add transaction manager support

--- cryptocurrency_parser/src/application/interactors/currency/add_currency.py
+++ cryptocurrency_parser/src/application/interactors/currency/add_currency.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from typing import Optional
 
 from application.common.interactor import Interactor
+from application.common.transaction_manager import TransactionManager
 from application.currency.currency_gateway import CurrencyWriter
 from cryptocurrency_parser.src.domain.models.currency.currency import Currency
 from domain.services.currency.currency import CurrencyService
@@ -17,10 +18,14 @@ class NewCurrencyDTO:
 
 class AddCurrency(Interactor[NewCurrencyDTO, None]):
     def __init__(
-        self, currency_db_gateway: CurrencyWriter, currency_service: CurrencyService
+        self,
+        currency_db_gateway: CurrencyWriter,
+        currency_service: CurrencyService,
+        transaction_manager: TransactionManager,
     ) -> None:
         self._currency_db_gateway = currency_db_gateway
         self._currency_service = currency_service
+        self._transaction_manager = transaction_manager
 
     async def __call__(self, data: NewCurrencyDTO) -> None:
         new_currency: Currency = self._currency_service.create_currency(
@@ -29,3 +34,4 @@ class AddCurrency(Interactor[NewCurrencyDTO, None]):
         )
 
         await self._currency_db_gateway.save_currency(new_currency)
+        await self._transaction_manager.commit()
