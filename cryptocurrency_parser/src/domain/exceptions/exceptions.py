153c64e5379074b21aecefb6d257c76664bacd21
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-02 23:45:09 +0200

    add exceptions to the domain

---
+++ cryptocurrency_parser/src/domain/exceptions/exceptions.py
@@ -0,0 +1,7 @@
+class EntityNotFoundError(Exception):
+    """Raised when an entity cannot be found by its identifier."""
+    def __init__(self, entity_type: str, entity_id: int):
+        self.entity_type = entity_type
+        self.entity_id = entity_id
+        self.message = f"{entity_type} with ID {entity_id} not found"
+        super().__init__(self.message)
