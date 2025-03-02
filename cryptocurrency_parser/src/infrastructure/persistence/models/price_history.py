3e3c123418fb685f24be14d878c8a6fb9dbc0d03
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-02 22:38:05 +0200

    fix typo in foreign key argument

--- cryptocurrency_parser/src/infrastructure/persistence/models/price_history.py
+++ cryptocurrency_parser/src/infrastructure/persistence/models/price_history.py
@@ -12,7 +12,7 @@ class PriceHistory(Base):
     __tablename__ = "price_history"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    currency_id: Mapped[int] = mapped_column(ForeignKey("currency.id"), nullable=False)
+    currency_id: Mapped[int] = mapped_column(ForeignKey("currencies.id"), nullable=False)
     updated_at: Mapped[datetime] = mapped_column(nullable=False)
     market_cap: Mapped[Decimal] = mapped_column(Numeric(precision=24, scale=16) ,nullable=False)
     market_cap_dominance: Mapped[float] = mapped_column(nullable=False)
