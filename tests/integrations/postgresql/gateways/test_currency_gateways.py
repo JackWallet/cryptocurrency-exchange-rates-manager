9df0038b0d59c64ae2dbf32186fc4de620343dc4
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-03 22:59:58 +0200

    remove src module

--- tests/integrations/postgresql/gateways/test_currency_gateways.py
+++ tests/integrations/postgresql/gateways/test_currency_gateways.py
@@ -1,8 +1,8 @@
 import pytest
 
-from cryptocurrency_parser.src.application.common.transaction_manager import TransactionManager
-from cryptocurrency_parser.src.application.currency.currency_gateway import CurrencyReader, CurrencyRemover, CurrencyWriter
-# from cryptocurrency_parser.src.infrastructure.database.currency.currency_gateways import 
+from cryptocurrency_parser.application.common.transaction_manager import TransactionManager
+from cryptocurrency_parser.application.currency.currency_gateway import CurrencyReader, CurrencyRemover, CurrencyWriter
+# from cryptocurrency_parser.infrastructure.database.currency.currency_gateways import 
 
 
 @pytest.fixture
