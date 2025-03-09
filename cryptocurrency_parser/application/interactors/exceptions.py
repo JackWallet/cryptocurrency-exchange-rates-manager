1e13aa201dfa6410902d7a3557b365f4c654008c
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-09 22:49:38 +0200

    refactor: add default init for two exceptions

--- cryptocurrency_parser/application/interactors/exceptions.py
+++ cryptocurrency_parser/application/interactors/exceptions.py
@@ -1,25 +1,21 @@
 class EntityNotFoundError(ValueError):
-    def __init__(self, entity_name: str, entity_id: str) -> None:
-        self._entity_name = entity_name
+    def __init__(self, entity_id: str) -> None:
         self._entity_id = entity_id
-
-    def __str__(self) -> str:
-        return (
-            f"The {self._entity_name} with id {self._entity_id} wasn't found"
-        )
 
 
 class PriceHistoryRecordNotFoundError(EntityNotFoundError):
-    def __init__(self, entity_name: str, entity_id: str) -> None:
-        super().__init__(entity_name, entity_id)
+    def __init__(self, entity_id: str) -> None:
+        super().__init__(entity_id)
 
     def __str__(self) -> str:
-        return super().__str__()
+        return (
+            f"The price history record with id {self._entity_id} wasn't found"
+        )
 
 
 class CurrencyNotFoundError(EntityNotFoundError):
-    def __init__(self, entity_name: str, entity_id: str) -> None:
-        super().__init__(entity_name, entity_id)
+    def __init__(self, entity_id: str) -> None:
+        super().__init__(entity_id)
 
     def __str__(self) -> str:
-        return super().__str__()
+        return f"Currency with id {self._entity_id} wasn't found"
