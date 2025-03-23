189d7bbd948c01342d42803835e158fc1afdbf46
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-23 16:50:43 +0200

    add price history getter handler

---
+++ cryptocurrency_parser/presentation/handlers.py
@@ -0,0 +1,36 @@
+from dishka.integrations.fastapi import FromDishka, inject
+from fastapi import APIRouter, HTTPException, status
+
+from cryptocurrency_parser.application.interactors.exceptions import (
+    PriceHistoryRecordNotFoundError,
+)
+from cryptocurrency_parser.application.interactors.price_history.get_price_history_by_id import (
+    GetPriceHistoryById,
+    GetPriceHistoryDTO,
+    PriceHistoryResultDTO,
+)
+from cryptocurrency_parser.domain.models.price_history.price_history_id import (
+    PriceHistoryId,
+)
+
+start_router = APIRouter()
+
+
+@start_router.get(path="/price_record/{price_record_id}")
+@inject
+async def get_price_history(
+    price_record_id: int,
+    interactor: FromDishka[GetPriceHistoryById],
+) -> PriceHistoryResultDTO:
+    try:
+        return await interactor(
+            data=GetPriceHistoryDTO(
+                price_history_id=PriceHistoryId(price_record_id),
+            ),
+        )
+    except PriceHistoryRecordNotFoundError as ex:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Item not found",
+            headers={"X-Error": str(ex)},
+        ) from ex
