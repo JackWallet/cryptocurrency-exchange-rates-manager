485f989dc6d363a0d35372935338a7b4daf41e6d
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-23 20:38:11 +0200

    tests init

--- tests/integrations/postgresql/gateways/test_currency_gateways.py
+++ tests/integrations/postgresql/gateways/test_currency_gateways.py
@@ -1,10 +1,10 @@
 import pytest
 
 from cryptocurrency_parser.application.common.transaction_manager import TransactionManager
-from cryptocurrency_parser.application.currency.currency_gateway import CurrencyReader, CurrencyRemover, CurrencyWriter
+from cryptocurrency_parser.application.currency.currency_gateway import CurrencyReader, CurrencyRemover, CurrencyAdder
 # from cryptocurrency_parser.infrastructure.database.currency.currency_gateways import 
 
 
-@pytest.fixture
-def user_reader_gateway() -> CurrencyReader:
-    ...
+# @pytest.fixture
+# def user_reader_gateway() -> CurrencyReader:
+#     ...
