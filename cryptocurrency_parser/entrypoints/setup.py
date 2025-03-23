a9c66a797aee8ae974c936b4c7cd28b79f52c738
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-23 17:52:55 +0200

    Add fastapi app getter with lifespan manager

--- cryptocurrency_parser/entrypoints/setup.py
+++ cryptocurrency_parser/entrypoints/setup.py
@@ -1,8 +1,24 @@
-from collections.abc import Iterable
+from collections.abc import AsyncGenerator, Iterable
+from contextlib import asynccontextmanager
 
 from dishka import AsyncContainer, Provider, make_async_container
+from fastapi import FastAPI
 
 from cryptocurrency_parser.entrypoints.config import Config, load_config
+from cryptocurrency_parser.presentation.handlers import start_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
+    yield
+    await app.state.dishka_container.close()
+
+
+def get_fastapi_app() -> FastAPI:
+    fastapi_app = FastAPI(lifespan=lifespan)
+    fastapi_app.include_router(start_router)
+
+    return fastapi_app
 
 
 def create_async_container(
