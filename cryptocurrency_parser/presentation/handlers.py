f6920856bd842d3d69d0dbe72ee1dc529f22a5a6
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-24 23:38:48 +0200

    Add geter by currency id DTO

--- cryptocurrency_parser/presentation/handlers.py
+++ cryptocurrency_parser/presentation/handlers.py
@@ -4,11 +4,17 @@ from fastapi import APIRouter, HTTPException, status
 from cryptocurrency_parser.application.interactors.exceptions import (
     PriceHistoryRecordNotFoundError,
 )
+from cryptocurrency_parser.application.interactors.price_history.get_price_history_by_currency_id import (
+    GetPriceHistoryByCurrencyId,
+    GetPriceHistoryByCurrencyIdDTO,
+    GetPriceHistoryByCurrencyIdResultDTO,
+)
 from cryptocurrency_parser.application.interactors.price_history.get_price_history_by_id import (
     GetPriceHistoryById,
-    GetPriceHistoryDTO,
-    PriceHistoryResultDTO,
+    GetPriceHistoryByIdDTO,
+    GetPriceHistoryResultByIdDTO,
 )
+from cryptocurrency_parser.domain.models.currency.currency_id import CurrencyId
 from cryptocurrency_parser.domain.models.price_history.price_history_id import (
     PriceHistoryId,
 )
@@ -21,11 +27,31 @@ start_router = APIRouter()
 async def get_price_history(
     price_record_id: int,
     interactor: FromDishka[GetPriceHistoryById],
-) -> PriceHistoryResultDTO:
+) -> GetPriceHistoryResultByIdDTO:
     try:
         return await interactor(
-            data=GetPriceHistoryDTO(
+            data=GetPriceHistoryByIdDTO(
                 price_history_id=PriceHistoryId(price_record_id),
+            ),
+        )
+    except PriceHistoryRecordNotFoundError as ex:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=str(ex),
+            headers={"X-Error": "Item not found"},
+        ) from ex
+
+
+@start_router.get(path="/price_record/by_currency/{currency_id}")
+@inject
+async def get_by_currency_id(
+    currency_id: int,
+    interactor: FromDishka[GetPriceHistoryByCurrencyId],
+) -> GetPriceHistoryByCurrencyIdResultDTO:
+    try:
+        return await interactor(
+            data=GetPriceHistoryByCurrencyIdDTO(
+                currency_id=CurrencyId(currency_id),
             ),
         )
     except PriceHistoryRecordNotFoundError as ex:
