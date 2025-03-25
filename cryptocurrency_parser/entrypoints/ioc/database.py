9a5bc3e36593011a6f845d2c3f78bb3e64ff75f3
InWamos <mykyta.pidgornyi@gmail.com>
2025-03-25 23:41:27 +0200

    Change provides to interface from explicit declaration

--- cryptocurrency_parser/entrypoints/ioc/database.py
+++ cryptocurrency_parser/entrypoints/ioc/database.py
@@ -1,8 +1,9 @@
-from collections.abc import AsyncGenerator
+from collections.abc import AsyncIterable
 
 from dishka import Provider, Scope, from_context, provide
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from cryptocurrency_parser.application.common.database import Database
 from cryptocurrency_parser.entrypoints.config import Config
 from cryptocurrency_parser.infrastructure.database.database import (
     SQLAlchemyDatabase,
@@ -16,13 +17,13 @@ class SQLAlchemyDatabaseProvider(Provider):
     def provide_database_engine(
         self,
         config: Config,
-    ) -> SQLAlchemyDatabase:
+    ) -> Database[AsyncSession]:
         return SQLAlchemyDatabase(config.postgres_config)
 
     @provide(scope=Scope.REQUEST)
     async def provide_session(
         self,
-        database: SQLAlchemyDatabase,
-    ) -> AsyncGenerator[AsyncSession, None]:
+        database: Database[AsyncSession],
+    ) -> AsyncIterable[AsyncSession]:
         async with database.get_session() as session:
             yield session
