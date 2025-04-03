dd20f0c52e68c54c38447a5f6aac19300f769b7b
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-03 23:16:22 +0300

    add mock class for currency

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -5,6 +5,7 @@ from unittest.mock import AsyncMock
 import pytest
 import pytest_asyncio
 
+from cryptocurrency_parser.domain.models.currency.currency import Currency
 from cryptocurrency_parser.domain.models.currency.currency_id import CurrencyId
 from cryptocurrency_parser.domain.models.price_history.price_history import (
     PriceHistory,
@@ -27,6 +28,18 @@ async def adder() -> AsyncMock:
 @pytest_asyncio.fixture
 async def remover() -> AsyncMock:
     return AsyncMock()
+
+
+@pytest.fixture
+def mock_currency() -> Currency:
+    return Currency(
+        id=CurrencyId(1221),
+        ticker="BTC",
+        full_name="bitcoin",
+        circulating_supply=121,
+        last_updated=datetime.now(tz=UTC),
+        max_supply=12,
+    )
 
 
 @pytest.fixture
