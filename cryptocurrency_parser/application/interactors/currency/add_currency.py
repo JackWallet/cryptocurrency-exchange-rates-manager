d51653fc3904f69e460662440b0ee725be77993e
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-04 00:17:06 +0300

    add if_exists check to interactor before calling saver

--- cryptocurrency_parser/application/interactors/currency/add_currency.py
+++ cryptocurrency_parser/application/interactors/currency/add_currency.py
@@ -6,6 +6,10 @@ from cryptocurrency_parser.application.common.transaction_manager import (
 )
 from cryptocurrency_parser.application.currency.currency_gateway import (
     CurrencyAdder,
+    CurrencyReader,
+)
+from cryptocurrency_parser.application.currency.exceptions import (
+    CurrencyTickerAlreadyInDatabaseError,
 )
 from cryptocurrency_parser.domain.models.currency.currency import Currency
 
@@ -21,13 +25,23 @@ class NewCurrencyDTO:
 class AddCurrency(Interactor[NewCurrencyDTO, None]):
     def __init__(
         self,
+        currency_reader: CurrencyReader,
         currency_adder: CurrencyAdder,
         transaction_manager: TransactionManager,
     ) -> None:
         self._currency_adder = currency_adder
         self._transaction_manager = transaction_manager
+        self._currency_reader = currency_reader
 
     async def __call__(self, data: NewCurrencyDTO) -> None:
+        if (
+            await self._currency_reader.get_currency_by_ticker(
+                currency_ticker=data.ticker,
+            )
+            is not None
+        ):
+            raise CurrencyTickerAlreadyInDatabaseError(ticker=data.ticker)
+
         new_currency: Currency = Currency.create(
             ticker=data.ticker,
             full_name=data.full_name,
