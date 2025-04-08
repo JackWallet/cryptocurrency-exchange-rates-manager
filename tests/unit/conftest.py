0abef9e3ddd93a9ada56e91bdedbee9d60e07b2c
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-08 21:31:08 +0300

    move session scope fixtures to the top

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -15,6 +15,38 @@ from cryptocurrency_parser.domain.models.price_history.price_history_id import (
 )
 
 
+@pytest.fixture(scope="session")
+def mock_currency() -> Currency:
+    return Currency(
+        id=CurrencyId(1221),
+        ticker="BTC",
+        full_name="bitcoin",
+        circulating_supply=121,
+        last_updated=datetime.now(tz=UTC),
+        max_supply=12,
+    )
+
+
+@pytest.fixture(scope="session")
+def mock_price_history() -> PriceHistory:
+    return PriceHistory(
+        id=PriceHistoryId(123),
+        currency_id=CurrencyId(1),
+        updated_at=datetime.now(tz=UTC),
+        circulating_supply=1,
+        market_cap=Decimal(1),
+        market_cap_dominance=1,
+        price=Decimal(1),
+        percent_change_1h=1.0,
+        percent_change_24h=1.0,
+        percent_change_30d=1.0,
+        percent_change_60d=1.0,
+        percent_change_7d=1.0,
+        percent_change_90d=1.0,
+        volume_24h=Decimal(1),
+    )
+
+
 @pytest_asyncio.fixture
 async def reader_not_found() -> AsyncMock:
     reader = AsyncMock()
@@ -43,38 +75,6 @@ async def remover() -> AsyncMock:
     remover = AsyncMock()
     remover.remove_currency = AsyncMock(return_type=None)
     return remover
-
-
-@pytest.fixture(scope="session")
-def mock_currency() -> Currency:
-    return Currency(
-        id=CurrencyId(1221),
-        ticker="BTC",
-        full_name="bitcoin",
-        circulating_supply=121,
-        last_updated=datetime.now(tz=UTC),
-        max_supply=12,
-    )
-
-
-@pytest.fixture
-def mock_price_history() -> PriceHistory:
-    return PriceHistory(
-        id=PriceHistoryId(123),
-        currency_id=CurrencyId(1),
-        updated_at=datetime.now(tz=UTC),
-        circulating_supply=1,
-        market_cap=Decimal(1),
-        market_cap_dominance=1,
-        price=Decimal(1),
-        percent_change_1h=1.0,
-        percent_change_24h=1.0,
-        percent_change_30d=1.0,
-        percent_change_60d=1.0,
-        percent_change_7d=1.0,
-        percent_change_90d=1.0,
-        volume_24h=Decimal(1),
-    )
 
 
 @pytest_asyncio.fixture
