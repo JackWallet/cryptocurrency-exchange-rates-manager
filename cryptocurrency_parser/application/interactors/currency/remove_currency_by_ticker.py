d01025344be834c88d89c2e9f5126df0f6e67429
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-09 18:37:31 +0300

    add None check before calling remove

--- cryptocurrency_parser/application/interactors/currency/remove_currency_by_ticker.py
+++ cryptocurrency_parser/application/interactors/currency/remove_currency_by_ticker.py
@@ -5,8 +5,10 @@ from application.common.transaction_manager import (
     TransactionManager,
 )
 from application.currency.currency_gateway import (
+    CurrencyReader,
     CurrencyRemover,
 )
+from application.currency.exceptions import CurrencyNotFoundByTickerError
 
 
 @dataclass(frozen=True)
@@ -17,13 +19,25 @@ class RemoveCurrencyByTickerDTO:
 class RemoveCurrencyByTicker(Interactor[RemoveCurrencyByTickerDTO, None]):
     def __init__(
         self,
+        currency_reader: CurrencyReader,
         currency_remover: CurrencyRemover,
         transaction_manager: TransactionManager,
     ) -> None:
+        self._currency_reader = currency_reader
         self._currency_remover = currency_remover
         self._transaction_manager = transaction_manager
 
     async def __call__(self, data: RemoveCurrencyByTickerDTO) -> None:
+        if (
+            await self._currency_reader.get_currency_by_ticker(
+                currency_ticker=data.currency_ticker,
+            )
+            is None
+        ):
+            raise CurrencyNotFoundByTickerError(
+                identifier=data.currency_ticker,
+            )
+
         await self._currency_remover.remove_currency_by_ticker(
             currency_ticker=data.currency_ticker,
         )
