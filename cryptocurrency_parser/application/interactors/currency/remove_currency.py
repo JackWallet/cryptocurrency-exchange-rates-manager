b17d479fd31717d1893e09f86efbb80ee26bcf6d
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-09 16:36:26 +0300

    rename gateway attribute

--- cryptocurrency_parser/application/interactors/currency/remove_currency.py
+++ cryptocurrency_parser/application/interactors/currency/remove_currency.py
@@ -18,14 +18,14 @@ class RemoveCurrencyDTO:
 class RemoveCurrency(Interactor[RemoveCurrencyDTO, None]):
     def __init__(
         self,
-        currency_db_gateway: CurrencyRemover,
+        currency_remover: CurrencyRemover,
         transaction_manager: TransactionManager,
     ) -> None:
-        self._currency_db_gateway = currency_db_gateway
+        self._currency_remover = currency_remover
         self._transaction_manager = transaction_manager
 
     async def __call__(self, data: RemoveCurrencyDTO) -> None:
-        await self._currency_db_gateway.remove_currency(
+        await self._currency_remover.remove_currency(
             currency_id=data.currency_id,
         )
         await self._transaction_manager.commit()
