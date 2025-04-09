2ff415b760317196f698913200610b60ebc48c09
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-09 18:31:59 +0300

    add None check before removing currency from the database by id

--- cryptocurrency_parser/application/interactors/currency/remove_currency_by_id.py
+++ cryptocurrency_parser/application/interactors/currency/remove_currency_by_id.py
@@ -5,8 +5,10 @@ from application.common.transaction_manager import (
     TransactionManager,
 )
 from application.currency.currency_gateway import (
+    CurrencyReader,
     CurrencyRemover,
 )
+from application.currency.exceptions import CurrencyNotFoundByIdError
 from domain.models.currency.currency_id import CurrencyId
 
 
@@ -19,12 +21,22 @@ class RemoveCurrencyById(Interactor[RemoveCurrencyByIdDTO, None]):
     def __init__(
         self,
         currency_remover: CurrencyRemover,
+        currency_reader: CurrencyReader,
         transaction_manager: TransactionManager,
     ) -> None:
         self._currency_remover = currency_remover
         self._transaction_manager = transaction_manager
+        self._currency_reader = currency_reader
 
     async def __call__(self, data: RemoveCurrencyByIdDTO) -> None:
+        if (
+            await self._currency_reader.get_currency_by_id(
+                currency_id=data.currency_id,
+            )
+            is None
+        ):
+            raise CurrencyNotFoundByIdError(identifier=data.currency_id)
+
         await self._currency_remover.remove_currency_by_id(
             currency_id=data.currency_id,
         )
