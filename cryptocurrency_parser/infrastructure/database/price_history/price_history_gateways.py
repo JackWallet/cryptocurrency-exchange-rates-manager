ee70d69ee350f96b62b5066054663c73b4ffd968
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-11 18:29:50 +0300

    remove mapper, adapt getter by currency id to imperative mapping

--- cryptocurrency_parser/infrastructure/database/price_history/price_history_gateways.py
+++ cryptocurrency_parser/infrastructure/database/price_history/price_history_gateways.py
@@ -1,3 +1,5 @@
+from collections.abc import Sequence
+
 from sqlalchemy import and_, delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,7 +16,7 @@ from domain.models.price_history.price_history_id import (
     PriceHistoryId,
 )
 from infrastructure.persistence.models.price_history import (
-    PriceHistoryModel,
+    price_history_table,
 )
 
 
@@ -22,56 +24,29 @@ class SQLAlchemyPriceHistoryReader(PriceHistoryReader):
     def __init__(self, session: AsyncSession) -> None:
         self._session = session
 
-    def _to_domain(
-        self,
-        price_history_model: PriceHistoryModel,
-    ) -> PriceHistory:
-        return PriceHistory(
-            id=PriceHistoryId(price_history_model.id),
-            currency_id=CurrencyId(price_history_model.currency_id),
-            updated_at=price_history_model.updated_at,
-            market_cap=price_history_model.market_cap,
-            market_cap_dominance=price_history_model.market_cap_dominance,
-            price=price_history_model.price,
-            volume_24h=price_history_model.volume_24h,
-            circulating_supply=price_history_model.circulating_supply,
-            percent_change_1h=price_history_model.percent_change_1h,
-            percent_change_24h=price_history_model.percent_change_24h,
-            percent_change_7d=price_history_model.percent_change_7d,
-            percent_change_30d=price_history_model.percent_change_30d,
-            percent_change_60d=price_history_model.percent_change_60d,
-            percent_change_90d=price_history_model.percent_change_90d,
-        )
-
     async def get_price_history_by_id(
         self,
         price_history_id: PriceHistoryId,
     ) -> PriceHistory | None:
-        query = select(PriceHistoryModel).where(
-            PriceHistoryModel.id == price_history_id,
-        )
-        query_result = await self._session.execute(query)
-        price_history = query_result.scalar_one_or_none()
-        return (
-            self._to_domain(price_history_model=price_history)
-            if price_history
-            else None
-        )
+        return await self._session.get(PriceHistory, price_history_id)
 
     async def get_price_history_by_currency_id(
         self,
         currency_id: CurrencyId,
     ) -> list[PriceHistory] | None:
-        query = select(PriceHistoryModel).where(
-            PriceHistoryModel.currency_id == currency_id,
-        ).limit(10)
-        query_result = await self._session.execute(query)
-        price_history = query_result.scalars()
-        return (
-            [self._to_domain(record) for record in list(price_history)]
-            if price_history
-            else None
+        query = (
+            select(PriceHistory)
+            .where(
+                price_history_table.c.currency_id == currency_id,
+            )
+            .limit(10)
         )
+        query_result = await self._session.execute(query)
+        scalars: Sequence[PriceHistory] = query_result.scalars().all()
+        if len(scalars) == 0:
+            return None
+
+        return list(scalars)
 
     async def get_price_history_by_currency_ids(
         self,
