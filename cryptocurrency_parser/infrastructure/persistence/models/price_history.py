c77390dd21e9fb4e64668054dd1b7ba3f1c4c5e6
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-11 14:33:12 +0300

    migrate to imperative tables

--- cryptocurrency_parser/infrastructure/persistence/models/price_history.py
+++ cryptocurrency_parser/infrastructure/persistence/models/price_history.py
@@ -1,34 +1,38 @@
-from datetime import datetime
-from decimal import Decimal
+from sqlalchemy import (
+    Column,
+    DateTime,
+    Float,
+    ForeignKey,
+    Integer,
+    Numeric,
+    Table,
+    func,
+)
 
-from sqlalchemy import ForeignKey, Numeric
-from sqlalchemy.orm import Mapped, mapped_column
+from domain.models.price_history.price_history import PriceHistory
+from infrastructure.persistence.models.base import mapper_registry
 
-from infrastructure.persistence.models.base import Base
+price_history_table = Table(
+    "price_history",
+    mapper_registry.metadata,
+    Column("price_history", primary_key=True, autoincrement=True),
+    Column(
+        "currency_id",
+        ForeignKey("currencies.id"),
+        nullable=False,
+    ),
+    Column("updated_at", DateTime, onupdate=func.now(), nullable=False),
+    Column("market_cap", Numeric(precision=24, scale=16), nullable=False),
+    Column("market_cap_dominance", Float, nullable=False),
+    Column("price", Numeric(precision=24, scale=24), nullable=False),
+    Column("volume_24h", Numeric(precision=24, scale=16), nullable=False),
+    Column("circulating_supply", Integer),
+    Column("percent_change_1h", Float, nullable=False),
+    Column("percent_change_24h", Float, nullable=False),
+    Column("percent_change_30d", Float, nullable=False),
+    Column("percent_change_60d", Float, nullable=False),
+    Column("percent_change_7d", Float, nullable=False),
+    Column("percent_change_90d", Float, nullable=False),
+)
 
-
-class PriceHistoryModel(Base):
-    __tablename__ = "price_history"
-
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    currency_id: Mapped[int] = mapped_column(
-        ForeignKey("currencies.id"), nullable=False,
-    )
-    updated_at: Mapped[datetime] = mapped_column(nullable=False)
-    market_cap: Mapped[Decimal] = mapped_column(
-        Numeric(precision=24, scale=16), nullable=False,
-    )
-    market_cap_dominance: Mapped[float] = mapped_column(nullable=False)
-    price: Mapped[Decimal] = mapped_column(
-        Numeric(precision=24, scale=24), nullable=False,
-    )
-    volume_24h: Mapped[Decimal] = mapped_column(
-        Numeric(precision=24, scale=16), nullable=False,
-    )
-    circulating_supply: Mapped[int]
-    percent_change_1h: Mapped[float] = mapped_column(nullable=False)
-    percent_change_24h: Mapped[float] = mapped_column(nullable=False)
-    percent_change_30d: Mapped[float] = mapped_column(nullable=False)
-    percent_change_60d: Mapped[float] = mapped_column(nullable=False)
-    percent_change_7d: Mapped[float] = mapped_column(nullable=False)
-    percent_change_90d: Mapped[float] = mapped_column(nullable=False)
+mapper_registry.map_imperatively(PriceHistory, price_history_table)
